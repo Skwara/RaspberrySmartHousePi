5da53c8c2c8f519eb82e1857c77199818a808647
skwarczy <skwara0@gmail.com>
2015-08-16 15:04:29 +0200

    added filling missing data

--- classes/DatabaseHandler.py
+++ classes/DatabaseHandler.py
@@ -36,7 +36,7 @@ class DatabaseHandler:
     def get_last_period_temperature(self, period_string):
         connection = sqlite3.connect(self.db_name)
         c = connection.cursor()
-        from_datetime = datetime.utcnow()
+        from_datetime = datetime.utcnow().replace(second=0, microsecond=0)
         if period_string == "HOUR":
             from_datetime -= timedelta(hours=1)
         elif period_string == "DAY":
@@ -45,13 +45,25 @@ class DatabaseHandler:
             from_datetime -= timedelta(days=7)
         elif period_string == "MONTH":
             from_datetime -= timedelta(days=30)
+        now = datetime.utcnow().replace(second=0, microsecond=0)
         c.execute('''SELECT * FROM temperatures
                      WHERE timestamp >= {} AND timestamp <= {}'''.format(self.__get_timestamp(from_datetime),
-                                                                         self.__get_timestamp(datetime.now())))
+                                                                         self.__get_timestamp(now)))
         temp_tab = c.fetchall()
+        self.__fill_missing_data(temp_tab, from_datetime, now)
         connection.close()
         return temp_tab
 
     @staticmethod
     def __get_timestamp(dt):
         return int((dt - datetime(1970, 1, 1)) / timedelta(seconds=1))
+
+    @staticmethod
+    def __fill_missing_data(temp_tab, from_datetime, to_datetime):
+        index = 0
+        while from_datetime != to_datetime:
+            from_datetime += timedelta(minutes=1)
+            current_timestamp = DatabaseHandler.__get_timestamp(from_datetime)
+            if index == len(temp_tab) or current_timestamp < temp_tab[index][1]:
+                temp_tab.insert(index, ["-", current_timestamp])
+            index += 1
